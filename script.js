7b73ebe6e1f586b576f6c60dd7b902eaf49bbc04
Cheah Cheng Qian(Ethan) <128024988+cheahcq99@users.noreply.github.com>
2024-01-19 01:01:46 +0800

    Update scripter.js

--- script.js
+++ script.js
@@ -51,7 +51,7 @@ window.onscroll = () => {
 
  /*Typed js*/
  const typed = new Typed('.multiple-text',{
-    strings: ['Business Analyst', 'Data Analyst'],
+    strings: ['Data Engineer', 'Data Analyst'],
     typeSpeed: 100,
     backSpeed: 100,
     backDelay: 1000,
